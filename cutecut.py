f3aaa919b3f1c0234efdec5e130a4d8dd75ab880
approximatenumber <approximatenumber@gmail.com>
2016-04-28 23:19:30 +0300

    pdf

--- cutecut.py
+++ cutecut.py
@@ -1,29 +1,12 @@
 #!/usr/bin/env python
 
 import os
-import shutil
 import sys
 import imghdr
 from wand.image import Image
-from wand.display import display
 
 supported = ['jpeg', 'png', 'tiff']
 exported = 'exported'
-
-
-def crop(path, image):
-    with Image(filename=image) as orig:
-        # rotate and crop left part of image
-        with orig.convert('jpeg') as img:
-            img.rotate(90)  # rotate to 270 deg
-            img.crop(0, 0, int(img.width/2), img.height)
-            img.save(filename='%s/%s/%s_l.jpg' % (path, exported, os.path.splitext(image)[0]))
-        # rotate and crop right part of image
-        with orig.convert('jpeg') as img:
-            img.rotate(90)  # rotate to 270 deg)
-            img.crop(int(img.width/2), 0, img.width, img.height)
-            img.save(filename='%s/%s/%s_r.jpg' % (path, exported, os.path.splitext(image)[0]))
-    return image
 
 
 def _exit(message):
@@ -38,21 +21,44 @@ def get_images(path):
             if (os.path.isfile(os.path.join(path, file)) and
                (imghdr.what(os.path.join(path, file)) in supported)):
                     images.append(file)
-        if not images:
-            _exit('no images in %s' % path)
         return images
-    except FileNotFoundError as e:
+    except FileNotFoundError:
         _exit('"%s" doesn`t exist!' % path)
+    except PermissionError as e:
+        print(e)
+        pass
+
+
+def crop(path, image):
+    filepath = os.path.join(path, exported)
+    with Image(filename=image) as orig:
+        if orig.width/orig.height < 1:
+            print('it looks like image %s needs to be rotated...' % image)
+            orig.rotate(90)
+        # rotate and crop left part of image
+        with orig.convert('jpeg') as img:
+            img.crop(0, 0, int(img.width/2), img.height)
+            img.save(filename=os.path.join(filepath, os.path.splitext(image)[0]) + '_l.jpg')
+        # rotate and crop right part of image
+        with orig.convert('jpeg') as img:
+            img.crop(int(img.width/2), 0, img.width, img.height)
+            img.save(filename=os.path.join(filepath, os.path.splitext(image)[0]) + '_r.jpg')
+    return image
+
+
+def convert2pdf(images):
+    pass
 
 
 def main():
-    try:
-        path = sys.argv[1]
-    except IndexError as e:
+    if len(sys.argv) < 2 or '--help' in sys.argv[1]:
         _exit("Usage:\ncutecut.py <path-to-directory>")
-
-    if not os.path.exists(path + "/" + exported):
-        os.makedirs(path + "/" + exported)
+    else:
+        path = sys.argv[1]
+    if not get_images(path):
+        _exit('no images in %s' % path)
+    if not os.path.exists(os.path.join(path, exported)):
+        os.makedirs(os.path.join(path, exported))
     for image in get_images(path):
         crop(path, image)
 
